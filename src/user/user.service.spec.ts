e4643df0774cfc0b0a4d865ca988d458119d8ba7
Ahmet Furkan KIZIL <furkan.kizil@ug.bilkent.edu.tr>
2024-06-05 20:13:26 +0300

    Tests are added

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,15 +1,27 @@
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 describe('UserService', () => {
   let service: UserService;
+  let repository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
