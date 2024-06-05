011e8786933bf902e152d76a3c7970367c062117
Ahmet Furkan KIZIL <furkan.kizil@ug.bilkent.edu.tr>
2024-06-05 16:35:43 +0300

    Update npm dependencies and authentication is encrypted using .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,22 @@ import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './user/user.entity';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost', 
-      port: 3306, 
-      username: 'root', 
-      password: 'mkA23.jfk', 
-      database: 'my_app', 
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [User],
-      synchronize: true, 
+      synchronize: true,
     }),
     UserModule,
   ],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,22 +1,25 @@
-// user.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { MailerModule } from '@nestjs-modules/mailer';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     MailerModule.forRoot({
       transport: {
-        host: 'smtp.gmail.com',
-        port: 465,
+        host: process.env.EMAIL_HOST,
+        port: parseInt(process.env.EMAIL_PORT),
         secure: true, // Use true for port 465
         auth: {
-          user: 'beijetest@gmail.com',
-          pass: 'bcqorcetawfxklki',
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
         },
       },
     }),
